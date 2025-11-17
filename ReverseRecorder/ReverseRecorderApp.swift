50c7c5f06bc98c652a999134b7326fb3291735d2
JuseongMoon <37950716+JuseongMoon@users.noreply.github.com>
2025-11-17 21:34:52 +0900

    Initial commit

---
+++ ReverseRecorder/ReverseRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseRecorderApp.swift
+//  ReverseRecorder
+//
+//  Created by 문주성 on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ReverseRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
